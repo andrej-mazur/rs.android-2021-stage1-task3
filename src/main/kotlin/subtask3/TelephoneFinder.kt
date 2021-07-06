de63d72c6996c9c4d74e13125031e9f947d8fa13
Andrej Mazur <andrej.mazur@outlook.com>
2021-07-06 23:02:51 +0300

    feat: add third subtask

--- src/main/kotlin/subtask3/TelephoneFinder.kt
+++ src/main/kotlin/subtask3/TelephoneFinder.kt
@@ -2,8 +2,30 @@ package subtask3
 
 class TelephoneFinder {
 
-    // TODO: Complete the following function
     fun findAllNumbersFromGivenNumber(number: String): Array<String>? {
-        throw NotImplementedError("Not implemented")
+        val numbers = mutableSetOf<String>()
+        val numberArray = number.toCharArray();
+        for ((index, digit) in numberArray.withIndex()) {
+            val neighborDigits = neighborDigitsMap[digit] ?: return null
+            for (neighborDigit in neighborDigits) {
+                numbers.add(String(numberArray.copyOf().also { it[index] = neighborDigit }))
+            }
+        }
+        return numbers.toTypedArray()
+    }
+
+    companion object {
+        private val neighborDigitsMap = mapOf(
+            '1' to setOf('2', '4'),
+            '2' to setOf('1', '3', '5'),
+            '3' to setOf('2', '6'),
+            '4' to setOf('1', '5', '7'),
+            '5' to setOf('2', '4', '6', '8'),
+            '6' to setOf('3', '5', '9'),
+            '7' to setOf('4', '8'),
+            '8' to setOf('5', '7', '9', '0'),
+            '9' to setOf('6', '8'),
+            '0' to setOf('8')
+        )
     }
 }
