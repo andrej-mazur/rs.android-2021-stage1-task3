74fa1252ff451b908598405ab0a95ba7326348f1
Andrej Mazur <andrej.mazur@outlook.com>
2021-07-08 01:00:51 +0300

    feat: add first subtask

--- src/main/kotlin/subtask1/Combinator.kt
+++ src/main/kotlin/subtask1/Combinator.kt
@@ -2,8 +2,23 @@ package subtask1
 
 class Combinator {
 
-    // TODO: Complete the following function
     fun checkChooseFromArray(array: Array<Int>): Int? {
-        throw NotImplementedError("Not implemented")
+        val m = array[0]
+        val n = array[1]
+        for (k in 1..n) {
+            if (m * 1L == factorial(n) / (factorial(k) * factorial(n - k))) {
+                return k
+            }
+        }
+        return null
+    }
+
+    companion object {
+        private tailrec fun factorial(n: Int, accumulator: Long = 1L): Long =
+            when (n) {
+                0 -> 1
+                1 -> accumulator
+                else -> factorial(n - 1, n * accumulator)
+            }
     }
 }
